6879f84dd6f09a688303b724d525f195b28b6820
Ericka Ponce <erickaponce92@gmail.com>
2025-03-23 09:21:31 -0500

    add service to component and remove unused code

--- src/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,39 +1,23 @@
-import { Component, signal} from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CharacterLisComponent } from "../../app/components/dragonball/character-lis/character-lis.component";
 import { CharacterAddComponent } from "../../app/components/dragonball/character-add/character-add-component";
-
-interface Character {
-    id: number,
-    name: string,
-    power: number
-}
+import { DragonballService } from "../../app/services/dragonball.service";
 
 @Component({
-    templateUrl: './dragonball-super-page.component.html',
-    selector: 'dragonball-super',
-    imports: [
-        CharacterLisComponent,
-        CharacterAddComponent
-    ]
+	templateUrl: './dragonball-super-page.component.html',
+	selector: 'dragonball-super',
+	imports: [
+		CharacterLisComponent,
+		CharacterAddComponent
+	]
 })
 
 export class DragonballSuperPageComponent {
-    name = signal('');
-    power = signal(0);
+    // manera antigua de inyectar un servicio
+    // constructor (
+    //     public drangonballService: DragonballService
+    // ){}
 
-    characters = signal<Character[]>([
-        {id: 1, name: 'Goku', power: 9001},
-        {id: 2, name: 'Vegeta', power: 8001},
-    ])
-
-    // Se manda a llamar newCharacter, es decir se recibe como param
-    addCharacter(newCharacter: Character) {
-        // Para insertarlo:
-        this.characters.update((list) => [ ... list, newCharacter ])
-    }
-
-    resetFields() {
-        this.name.set('');
-        this.power.set(0)
-    }
+    // manera actual de inyectar un servicio
+    public dragonballService = inject(DragonballService);
 }
